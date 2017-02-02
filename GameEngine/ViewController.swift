c5c6fcff284148f3b9a4ec3721c3046c0179a3b6
SorataYuu <jeremyjds@me.com>
2017-02-02 17:15:41 +0800

    Initial Commit

---
+++ GameEngine/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GameEngine
+//
+//  Created by Jeremy Jee on 2/2/17.
+//  Copyright © 2017 nus.cs3217.a0139963n. All rights reserved.
+//
+
+import UIKit
+import Metal
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
